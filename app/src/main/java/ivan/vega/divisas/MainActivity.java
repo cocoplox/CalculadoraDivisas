1ca1801bb1e95acd2f80607b138798e5a4674375
= <=>
2024-10-29 21:23:57 +0100

    Ready for final check

--- app/src/main/java/ivan/vega/divisas/MainActivity.java
+++ app/src/main/java/ivan/vega/divisas/MainActivity.java
@@ -1,6 +1,8 @@
 package ivan.vega.divisas;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +17,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     double tasaCambio = 1.0;
@@ -67,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
          radioButtonLB = findViewById(R.id.radioButtonLibra);
          radioButtonYU = findViewById(R.id.radioButtonYuan);
 
+
+         radioButtonDOL.toggle();
         //La coma funciona algo diferente
 
         Button comaButton = findViewById(R.id.button11);
@@ -82,6 +90,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        //Aqui realmente viene la chicha
+
+        inputValor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //No lo vamos a usar
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //Cada vez que el texto cambio, queremos que pille el imput, lo convierta y lo muestre
+
+                resultado.setText(realizarCambioConversion(s.toString()));
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //No lo vamos a usar
+            }
+        });
+
+        redioGroupDivisas.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                String valorTexto = inputValor.getText().toString();
+                if(!valorTexto.isEmpty()){
+                    resultado.setText(realizarCambioConversion(valorTexto));
+                }
+
+            }
+        });
 
         Button ceButton = findViewById(R.id.button12);
         ceButton.setOnClickListener(new View.OnClickListener() {
@@ -127,16 +168,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void realizarCambioConversion(String valorTexto, RadioGroup radioGroupDivisas, TextView textoResultado){
+    private String realizarCambioConversion(String valorTexto){
         //Este metodo pido el valor actual del input, en que divisa de conversion se encuentra, y finalmente el resultado, para cambiarlo
+
+        double resultado = 0;
+        String suffix = "";
+
         if(!valorTexto.isEmpty()){
             //Convertimos a double para poder operar
             double valor = Double.parseDouble(valorTexto);
-            double resultado = 0;
 
+
+            //Seguramente lo cambia a un switch mas adelante
+            if(radioButtonDOL.isChecked()){
+                //Pillamos el imput y, lo convertimos y devolvemos
+                resultado = valor * EUR_TO_DOLL;
+                suffix = "UDS";
+            }
+            if(radioButtonLB.isChecked()){
+                resultado = valor * EUR_TO_LIB;
+                suffix = "GBP";
+            }
+            if(radioButtonYEN.isChecked()){
+                resultado = valor * EUR_TO_YEN;
+                suffix = "JPY";
+            }
+            if(radioButtonYU.isChecked()){
+                resultado = valor * EUR_TO_YUAN;
+                suffix = "CNY";
+            }
 
 
         }
+
+
+        //Esto sinceramente lo ha hecho GPT xk no tengo ni idea de formatear cosas
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+        String resultadoTexto = decimalFormat.format(resultado) + "  " + suffix;
+        return resultadoTexto;
     }
 
 }
